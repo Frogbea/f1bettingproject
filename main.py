8e9705763c55b3ebee3ecf76a59594ba450b5a4d
EMILY <emgriggs123@gmail.com>
2024-05-29 10:23:34 +1200

    pls work

---
+++ main.py
@@ -0,0 +1,12 @@
+import requests, json
+
+def jprint(obj):
+    text = json.dumps(obj, sort_keys=True, indent=4)
+    print(text)
+
+
+response = requests.get('http://ergast.com/api/f1/current/constructorStandings.json') 
+print(response.status_code)
+jprint(response.json())
+
+print('hello world')
