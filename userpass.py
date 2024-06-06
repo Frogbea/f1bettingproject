859ca01d95b7de39e47732d75327aed757ae5ec4
EMILY <emgriggs123@gmail.com>
2024-06-06 21:28:12 +1200

    trying my best

--- userpass.py
+++ userpass.py
@@ -1,7 +1,47 @@
 # USERNAME and password component
+#**********IMPORTS***************
 import csv
 
-with open("students.csv", 'r') as file:
+#********VARIBLES****************
+username = 'blank'
+password = 'blank'
+
+#*************FUNCTIONS*************
+def newUser():
+    username = input('Please enter a username:')
+    password = input('Please create a password')
+
+def previousUser():
+    print('sign in')
+
+
+
+
+# MAIN***************************************************************************************************
+while True:
+    choice = input('Do you want to:\n 1.Sign in\n 2.Sign up \n Type what you would like to do:').lower()
+    if choice =='sign in':
+        previousUser()
+        break
+    elif choice == 'sign up':
+        newUser()
+        break
+    else:
+        print('!ERROR! Please type in your command correctly.')
+   
+
+with open('users.csv', 'r') as file:
   csvreader = csv.reader(file)
   for row in csvreader:
     print(row)
+
+    
+
+with open('users.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    
+    writer.writerow(["SNo", "Name", "Subject"])
+    writer.writerow([1, "Ash Ketchum", "English"])
+    writer.writerow([2, "Gary Oak", "Mathematics"])
+    writer.writerow([3, "Brock Lesner", "Physics"])
+
