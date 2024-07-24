626f632874f156dcd1a1b7d1dccc175c30c99929
EMILY <emgriggs123@gmail.com>
2024-07-24 21:39:50 +1200

    change

--- apitest.py
+++ apitest.py
@@ -4,5 +4,5 @@ def jprint(obj):
     text = json.dumps(obj, sort_keys=True, indent=4)
     print(text)
 
-response = requests.get('http://ergast.com/api/f1/current') 
-print = jprint(response.json())
+response = requests.get('http://ergast.com/api/f1/current/constructorStandings.json') 
+prin = jprint(response.json())
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,8 @@
 import requests, json
+
+def jprint(obj):
+    text = json.dumps(obj, sort_keys=True, indent=4)
+    print(text)
+
+response = requests.get('http://ergast.com/api/f1/current') 
+print = jprint(response.json())
